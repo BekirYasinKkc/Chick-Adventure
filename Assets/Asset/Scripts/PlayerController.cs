a62d4ec1c30eb16a7f926bc350fe6ca22c04fbc9
Bekir Yasin Kökcü <bekirkokcu298@gmail.com>
2026-04-22 16:10:58 +0200

    v0.5 Charcter Gravity & Jump Settings Finished

--- Assets/Asset/Scripts/PlayerController.cs
+++ Assets/Asset/Scripts/PlayerController.cs
@@ -1,45 +1,84 @@
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class PlayerController : MonoBehaviour
+namespace BYK
 {
-    [SerializeField] private Transform orientationTransform;
-
-    [SerializeField] private float MovementSpeed;
-
-    private Rigidbody playerRigidbody;
-
-    private float horizontalInput, verticalInput;
-
-    private Vector3 MovementDirection;
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour
     {
-        playerRigidbody = GetComponent<Rigidbody>();
-        playerRigidbody.freezeRotation = true;
-    }
+        [Header("References")]
+        [SerializeField] private Transform orientationTransform;
 
-    private void Update()
-    {
-        SetInput();
-    }
+        [Header("Movement Settings")]
+        [SerializeField] private float MovementSpeed;
 
-    private void FixedUpdate()
-    {
-        SetPlayerMovement();
-    }
+        [Header("Jump Settings")]
+        [SerializeField] private KeyCode jumpKey;
+        [SerializeField] private float jumpForce;
+        [SerializeField] private float jumpCooldown;
+        [SerializeField] private bool canJump;
 
-    private void SetInput()
-    {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
-        verticalInput = Input.GetAxisRaw("Vertical");
-    }
+        [Header("Ground Check Settings")]
+        [SerializeField] private float playerHeight;
+        [SerializeField] private LayerMask groundLayer;
 
-    private void SetPlayerMovement()
-    {
-        MovementDirection = orientationTransform.forward * 
-            verticalInput + orientationTransform.right * horizontalInput;
 
-        playerRigidbody.AddForce(MovementDirection * MovementSpeed, ForceMode.Force);
+        private Rigidbody playerRigidbody;
+
+        private float horizontalInput, verticalInput;
+
+        private Vector3 MovementDirection;
+
+        private void Awake()
+        {
+            playerRigidbody = GetComponent<Rigidbody>();
+            playerRigidbody.freezeRotation = true;
+        }
+
+        private void Update()
+        {
+            SetInput();
+        }
+
+        private void FixedUpdate()
+        {
+            SetPlayerMovement();
+        }
+
+        private void SetInput()
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+            
+            if (Input.GetKey(jumpKey)&& canJump && IsGrounded())
+            {
+                canJump = false;
+                SetPlayerJumping();
+                Invoke(nameof(ResetJumping), jumpCooldown);
+            }
+        }
+
+        private void SetPlayerMovement()
+        {
+            MovementDirection = orientationTransform.forward *
+                verticalInput + orientationTransform.right * horizontalInput;
+
+            playerRigidbody.AddForce(MovementDirection.normalized * MovementSpeed, ForceMode.Force);
+        }
+
+        private void SetPlayerJumping()
+        {
+            playerRigidbody.linearVelocity = new Vector3(playerRigidbody.linearVelocity.x, 0f, playerRigidbody.linearVelocity.z);
+            playerRigidbody.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+        }
+
+        private void ResetJumping()
+        {
+            canJump = true;
+        }
+
+        private bool IsGrounded()
+        {
+            return Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
+        }
     }
-}
+}   
